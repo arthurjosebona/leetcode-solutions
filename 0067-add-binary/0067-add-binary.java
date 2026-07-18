2f6dd2917941c6d839051dd797c31eb9eb6fff18
arthurjosebona <arthurjosebona@gmail.com>
2026-07-18 12:45:09 -0300

    Time: 11 ms (6.62%), Space: 47.2 MB (5.22%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,52 +1,30 @@
+import java.math.BigInteger;
+
 class Solution {
     public String addBinary(String a, String b) {
-        Stack<Character> result = new Stack<>();
+        return toBin(
+            toDec(a).add(toDec(b))
+        );
+    }
 
-        int indexA = a.length() - 1;
-        int indexB = b.length() - 1;
-
-        boolean actA, actB;
-        boolean auxSum = false;
-
-        while(indexA>=0 || indexB>=0 || auxSum) {
-            actA = indexA >= 0 ? a.charAt(indexA--) == '1' : false;
-            actB = indexB >= 0 ? b.charAt(indexB--) == '1' : false;
-            if (actA && actB) {
-                if (auxSum) {
-                    System.out.println("1:1");
-                    result.push('1');
-                } else {
-                    auxSum = true;
-                    System.out.println("2:0");
-                    result.push('0');
-                }
-            } else if (actA || actB) {
-                if (auxSum) {
-                    System.out.println("3:0");
-                    result.push('0');
-                } else {
-                    System.out.println("4:1");
-                    result.push('1');
-                }
-            } else {
-                if (auxSum) {
-                    System.out.println("5:1");
-                    result.push('1');
-                    auxSum = false;
-                } else {
-                    System.out.println("6:0");
-                    result.push('0');
-                }
+    private BigInteger toDec(String bin) {
+        int size = bin.length();
+        BigInteger decSum = BigInteger.ZERO;
+        for (int i=0; i<size; i++) {
+            if (bin.charAt(i) == '1') {
+                decSum = decSum.add(BigInteger.TWO.pow(size - 1 - i));
             }
         }
+        return decSum;
+    }
 
-        String response = "";
-
-        int size = result.size();
-        for (int i=0; i<size; i++) {
-            response += result.pop();
+    private String toBin(BigInteger dec) {
+        String binResult = "";
+        BigInteger two = BigInteger.TWO;
+        while (!dec.equals(BigInteger.ZERO)) {
+            binResult = dec.mod(two) + binResult;
+            dec = dec.divide(two);
         }
-
-        return response;
+        return !binResult.equals("") ? binResult : "0";
     }
 }
