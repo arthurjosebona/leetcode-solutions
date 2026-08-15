028eb371bae168f3477dcbc7c150ad4daf462f5c
arthurjosebona <arthurjosebona@gmail.com>
2026-08-14 21:54:12 -0300

    Time: 0 ms (100%), Space: 46.2 MB (43.1%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.java
@@ -0,0 +1,8 @@
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        double[] answer = new double[2];
+        answer[0] = celsius + 273.15;
+        answer[1] = celsius * 1.8 + 32;
+        return answer;
+    }
+}
