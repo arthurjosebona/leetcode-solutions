61ce31bc8df728ae34771ffcc8bd4640464d9308
arthurjosebona <arthurjosebona@gmail.com>
2026-09-04 13:41:33 -0300

    Time: 1 ms (99.79%), Space: 46.4 MB (32.13%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -2,18 +2,34 @@ class Solution {
     public int firstStableIndex(int[] nums, int k) {
         int ans = -1;
 
+        int smallest = -1;
+        int biggest = -1;
+
         for (int i=0; i<nums.length; i++) {
-            int smallest = -1;
-            int biggest = -1;
-            for (int j=0; j<nums.length; j++) {
-                if (j <= i && (biggest == -1 || nums[j] > biggest)) {
-                    biggest = nums[j];
+            
+            if (i==0) {
+                biggest = 0;
+                smallest = 0;
+                for (int j=1; j<nums.length; j++) {
+                    if (nums[j] < nums[smallest]) {
+                        smallest = j;
+                    }
                 }
-                if (j >= i && (smallest == -1 || nums[j] < smallest)) {
-                    smallest = nums[j];
+            } else {
+                if (nums[i] > nums[biggest]) {
+                    biggest = i;
+                }
+                if (i - 1 == smallest) {
+                    smallest = i;
+                    for (int j=i+1; j<nums.length; j++) {
+                        if (nums[j] < nums[smallest]) {
+                            smallest = j;
+                        }
+                    }
                 }
             }
-            int sub = biggest - smallest;
+            
+            int sub = nums[biggest] - nums[smallest];
             if (sub <= k) {
                 ans = i;
                 break;
